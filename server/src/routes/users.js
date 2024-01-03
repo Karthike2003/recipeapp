2618655737c002978f981e5f5d000818145ab19f
Karthik <karthiksujith789@gmail.com>
2024-01-03 19:49:13 +0530

    successfully completed the first phase of backend ie register and login

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -10,16 +10,35 @@ const router = express.Router();
 
 
 router.post('/register', async (req, res) => {
-    // console.log(req)
+    
     const { username, password} = req.body;
    
     const user = await UserModel.findOne({username});
-    res.json(user);
-});
+    if (user) {
+        return res.json({message: "User already exists"});
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new UserModel({username, password: hashedPassword});
+    await newUser.save();
 
+    res.json({message: "User Registeres Successfully"});   
+    });
 
+ router.post('/login', async (req, res) => {
+        const { username, password} = req.body;
+        const user = await UserModel.findOne({username});
+        if(!user) {
+            return res.json({message: "User does not exist"});
+        }
 
+        const isPasswordValid = await bcrypt.compare(password, user.password);
 
+        if (!isPasswordValid) {
+            return res.json({message: "username or password is incorrect"});
+        }
 
+        const token = jwt.sign({id: user._id}, "secret");
+        res.json({token, userID: user._id,});
+    });
 
 export {router as userRouter};
